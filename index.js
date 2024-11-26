574ab277c9acebcab6e4e708f0989b193daf536a
ManralaDiaries <145805697+jaleev@users.noreply.github.com>
2024-11-26 17:42:35 -0500

    Code update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('GitOps Demo App with Node.js')
+  res.send('GitOps Demo App with Node.js!!!')
 })
 
 app.listen(port, () => {
